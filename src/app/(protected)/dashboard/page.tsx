9d9a71c958fa8a34178ad06ae174c9250545c1a2
hoangtrung1002 <n.jordan1002@gmail.com>
2024-12-16 16:28:12 +0700

    feat: fetch the information of user in clerk and create user in db

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import { onBoardUser } from "@/actions/user";
+import { redirect } from "next/navigation";
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
+const Dashboard = async () => {
+  const user = await onBoardUser();
+  const username = user && user.data?.email.split("@")[0];
+  if (user?.status === 200 || user?.status === 201) {
+    return redirect(`dashboard/${username}`);
+  }
+  return redirect("sign-in");
 };
 export default Dashboard;
